2ef5a99fe410d618c10e9a7914d29f2f2a4ed00d
Stephen Choo <stephen.w.choo@gmail.com>
2024-05-26 20:31:18 +1000

    Added Pop Up view to storybook

--- stories/PopUp.stories.ts
+++ stories/PopUp.stories.ts
@@ -1,10 +1,11 @@
 import type { Meta, StoryObj } from "@storybook/react"
-import { TopBar } from "../src/view/content/TopBar"
+import PopUp from "../src/view/popup/PopUp"
+import { Category } from "../src/data/models/SiteData"
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-	title: "Content View",
-	component: TopBar,
+	title: "Pop Up View",
+	component: PopUp,
 	parameters: {
 		// Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
 		layout: "centered",
@@ -14,15 +15,38 @@ const meta = {
 	// More on argTypes: https://storybook.js.org/docs/api/argtypes
 	// Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
 	args: {},
-} satisfies Meta<typeof TopBar>
+} satisfies Meta<typeof PopUp>
 
 export default meta
 
 type Story = StoryObj<typeof meta>
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const TopBarStory: Story = {
+export const PopUpErrorView: Story = {
 	args: {
-		serialisedSiteData: "",
-		siteStatus: { success: true },
+		modelMetricsVal: null,
+		siteDataStateVal: null,
+		siteCategoryVal: null,
+		siteClassificationStateVal: null,
+	},
+}
+
+export const PopUpExampleView: Story = {
+	args: {
+		modelMetricsVal: {
+			procrastination: 5,
+			productive: 10,
+			changesSinceLastSync: 0,
+		},
+		siteDataStateVal: {
+			domain: "example.com",
+			title: "Example",
+		},
+		siteCategoryVal: Category.productive,
+		siteClassificationStateVal: {
+			isProcrastinationSite: 0.5,
+			seenBefore: Category.productive,
+			success: true,
+			modelUntrained: false,
+		},
 	},
 }
